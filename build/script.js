1c93ac0e25be73a73f0a6659607f2ddb0693c3f5
alien_001 <103817982+deniskolabsynskyy@users.noreply.github.com>
2023-05-07 15:49:39 +0200

    Add existing file

--- build/script.js
+++ build/script.js
@@ -91,3 +91,154 @@ var hidden_list = document.querySelector('.container_for_tab .tab_content');
 tab(download_more, hidden_list);
 
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const services = [{
+//   id: 1,
+//   Type: "",
+//   Size: "L",
+//   Color: "Red"
+// },
+// {
+//   id: 2,
+//   Type: "Apple",
+//   Size: "S",
+//   Color: "Black"
+// },
+// {
+//   id: 3,
+//   Type: "Window",
+//   Size: "M",
+//   Color: "Black"
+// },
+// {
+//   id: 4,
+//   Type: "Linux",
+//   Size: "M",
+//   Color: "White"
+// },
+// {
+//   id: 5,
+//   Type: "Linux",
+//   Size: "S",
+//   Color: "Red"
+// },
+// {
+//   id: 6,
+//   Type: "Windows",
+//   Size: "S",
+//   Color: "Red"
+// },
+// ]
+
+// const findServices = (filters) => {
+// let answer = [...services];
+
+// for (const filter in filters) {
+//   const filterSet = filters[filter];
+ 
+//   answer = answer.filter(
+//     item => filterSet.size === 0 ||  filterSet.has(item[filter])
+//   );
+// }
+
+// document.querySelector('#selected').innerHTML = answer.map(item => `<p>${item.Type} ${item.Size} ${item.Color}</p>`).join('');
+
+// return answer;
+// }
+
+// const checkboxes = document.querySelectorAll('#form [type="checkbox"]');
+
+// const filters = {
+//   Type: new Set(),
+//   Size: new Set(),
+//   Color: new Set(),
+// };
+
+// checkboxes.forEach(checkbox => {
+// checkbox.addEventListener('change', (e) => {
+//   console.clear();
+  
+//   const target = e.target;
+//   const [prop, value] = target.name.split('-');
+
+//   filters[prop][target.checked ? 'add' : 'delete']('' + value);
+
+//   findServices(filters);
+// })
+// })
+
+// document.onload = findServices(filters);
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
+
+
+
+
+// GenerateGenres();
+
+function filterGames(genre) {
+  var games = document.getElementsByClassName("game");
+  if (genre == "all") genre = "";
+  for (var i = 0; i < games.length; i++) {
+      games[i].style.display = "none";
+      if (games[i].className.indexOf(genre) > -1) games[i].style.display = "block";
+  }
+}
+filterGames("all");
+
+
+
+
+const genre = {
+  name: 'Sports',
+  img: './img/bg-image-6.png'
+}
+
+const button = document.createElement('button')
+button.addEventListener('click', () => {
+  const basket = localStorage.getItem('shop-basket');
+
+  if(basket) {
+      const JSONBasket = JSON.parse(basket);
+      JSONBasket.push(genre);
+
+      localStorage.setItem('shop-basket', JSON.stringify(JSONBasket))
+  } else {
+      const arrBasket = [];
+
+      arrBasket.push(genre);
+
+      localStorage.setItem('shop-basket', JSON.stringify(arrBasket))
+  }
+
+  // при натисканні у вас вже може бути корзин
+  // при натисканні корзини може не бути
+
+})
+button.textContent = "BUY";
+
+const basket = JSON.parse(localStorage.getItem('shop-basket'));
